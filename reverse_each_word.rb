c70e5cb6a5aac4cbd144d6c1ebb2dde8d6efbdda
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 21:31:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ def reverse_each_word(string)
   # end
 
   string.reverse.split.reverse.join(" ") #reverse the string, then turn the string into an array, then reserve the elements in the array (back to how they were) and join back into a string, with spaces in between each word/array item
-
+end
 ## using collect
 
 # array = string.split
@@ -21,7 +21,9 @@ def reverse_each_word(string)
 #   x.reverse.join(" ")
 # end
 
-
+  string.split.collect do |x|
+    x.reverse
+  end
 
 end
 
