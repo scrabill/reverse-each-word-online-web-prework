272f4154d772cd858de5a1b717caf82ea8182c86
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 20:32:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,2 @@
-def reverse_each_word(string)
-  array = string.split
-
-  array.each do |array|
-    puts array.reverse
-  end
-
-  ## string = array.join
-
-end
-
-reverse_each_word("Hello there, and how are you?")
-reverse_each_word("This is another string!")
-
-def reverse_each_word(string)
-  array = string.split
-
-  array.collect do |array|
-    puts array.reverse
-  end
-
-  ## string = array.join
-
+def reverse_each_word
 end
