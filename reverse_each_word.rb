c0e08f7f20dddd8e4e916aed45eca663eff006fa
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 20:38:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 # def reverse_each_word(string)
 #   array = string.split
 # end
+
+def reverse_each_word(string)
+  array = string.split
+
+  array.each do |array|
+    puts array.reverse
+  end
+
+end
